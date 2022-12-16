b19b1c3902a869b519e3895d339a70d423f01927
andrepavanelli7 <pavanellidev@gmail.com>
2022-12-16 16:10:46 -0300

    training JS

---
+++ 06 Estruturas/js/script.js
@@ -0,0 +1,31 @@
+// 1 - variáveis
+
+let nome = "Andre";
+
+console.log(nome);
+
+nome = "Andre Pavanelli";
+
+console.log(nome);
+
+const idade = 31;
+
+console.log(idade);
+
+// 2 - mais sobre variáveis
+
+let a = 10, b = 20, c = 30;
+
+console.log(c, a ,b);
+
+const nomeCompleto = "André Pavanelli";
+
+const nomecompleto = "João da Silva";
+
+console.log(nomecompleto, nomeCompleto);
+
+// 3 - prompt
+
+const age = prompt("Digite a sua idade:");
+
+console.log(`Você tem ${age} anos.`);
