f0518160111733b19acc3b8fc72bcb7dbb4ce632
andrepavanelli7 <pavanellidev@gmail.com>
2022-12-30 09:44:30 -0300

    estudos 30/12/22

---
+++ 09 - DOM/index.js
@@ -0,0 +1,8 @@
+document.getElementsByTagName('li')[2].innerHTML = "André";
+
+document.querySelectorAll('li')[2].style.color = "#22ddcc";
+
+document.querySelector('li a').style.color = "red";
+
+document.querySelector('.btn').style.backgroundColor = "yellow";
+
